d3e86127b6f0ce3512747016c277b303042f38f0
ioanok <calculat1>
2016-08-14 01:25:10 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+symfony_auth
+=============
+
+A Symfony project created on August 13, 2016, 9:21 pm.
+
+https://auth0.com/blog/creating-your-first-symfony-app-and-adding-authentication/
---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: john
+ * Date: 13-Aug-16
+ * Time: 11:31 PM
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        $helper = $this->get('security.authentication_utils');
+
+        return $this->render(
+            'auth/login.html.twig',
+            array(
+                'last_username' => $helper->getLastUsername(),
+                'error'         => $helper->getLastAuthenticationError(),
+            )
+        );
+    }
+
+    /**
+     * @Route("/login_check", name="security_login_check")
+     */
+    public function loginCheckAction()
+    {
+
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+
+    }
+}
